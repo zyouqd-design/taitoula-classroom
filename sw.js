73ab4b2ee15fc2e9d63e9585e0940f1ca4397d0b
zyouqd-design <zyouqd@gmail.com>
2026-09-23 01:15:48 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='taitoula-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
